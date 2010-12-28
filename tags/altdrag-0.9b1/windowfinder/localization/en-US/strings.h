7a1a8bb138cda8a602081e6fd02e549e0cbf1871
recover89 <recover89@4a835583-4451-0410-ad23-d19e25570b06>
2010-12-28 14:20:52 +0000

    Tagging 0.9 beta 1 release.

---
+++ tags/altdrag-0.9b1/windowfinder/localization/en-US/strings.h
@@ -0,0 +1,32 @@
+/*
+	WindowFinder - en-US localization by Stefan Sundin (recover89@gmail.com)
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+*/
+
+struct strings en_US = {
+ /* menu_find      */ L"Find",
+ /* menu_finddelay */ L"Find in 3 seconds",
+ /* menu_findall   */ L"Enumerate all windows",
+ /* menu_about     */ L"About",
+ /* menu_exit      */ L"Exit",
+ /* wnddetails     */ L"Window details",
+ /* allwnds        */ L"Visible windows",
+ /* about_title    */ L"About " APP_NAME,
+ /* about          */ APP_NAME " - " APP_VERSION "\n\
+Made by Stefan Sundin\n\
+\n\
+Find window's title and classname for use in AltDrag's blacklist. \
+Click the tray icon and then click the window you want to blacklist. \
+A popup will appear with information about that window. \
+For more tricky windows, use the delayed find.\n\
+\n\
+Note that you can't blacklist windows in AltDrag by using the child or component information! \
+They are provided purely for informational purposes!\n\
+\n\
+Read more on wiki page: "APP_URL"\n\
+Visit the author's website at http://stefansundin.com/"
+};
