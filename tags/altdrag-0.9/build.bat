03e295c22efb24189d629276c74a3b927044b999
recover89 <recover89@4a835583-4451-0410-ad23-d19e25570b06>
2011-02-01 19:22:58 +0000

    Tagging 0.9 release.

---
+++ tags/altdrag-0.9/build.bat
@@ -0,0 +1,75 @@
+@echo off
+
+:: For traditional MinGW, set prefix32 to empty string
+:: For mingw-w32, set prefix32 to i686-w64-mingw32-
+:: For mingw-w64, set prefix64 to x86_64-w64-mingw32-
+
+set prefix32=i686-w64-mingw32-
+set prefix64=x86_64-w64-mingw32-
+set l10n=en-US es-ES gl-ES
+
+taskkill /IM AltDrag.exe
+
+if not exist build. mkdir build
+
+rem %prefix32%gcc -o build\unhook.exe include\unhook.c
+rem "build\unhook.exe"
+
+%prefix32%windres -o build\altdrag.o include\altdrag.rc
+%prefix32%windres -o build\hooks.o include\hooks.rc
+
+if "%1" == "all" (
+	%prefix32%gcc -o build\ini.exe include\ini.c -lshlwapi
+	
+	@echo.
+	echo Building binaries
+	%prefix32%gcc -o "build\AltDrag.exe" altdrag.c build\altdrag.o -mwindows -lshlwapi -lwininet -O2 -s
+	if not exist "build\AltDrag.exe". exit /b
+	%prefix32%gcc -o "build\hooks.dll" hooks.c build\hooks.o -mdll -lshlwapi -lcomctl32 -O2 -s
+	if not exist "build\hooks.dll". exit /b
+	
+	if "%2" == "x64" (
+		%prefix64%windres -o build\hookwindows_x64.o include\hookwindows_x64.rc
+		%prefix64%windres -o build\hooks_x64.o include\hooks.rc
+		%prefix64%gcc -o "build\HookWindows_x64.exe" hookwindows_x64.c build\hookwindows_x64.o -mwindows -lshlwapi -O2 -s
+		if not exist "build\HookWindows_x64.exe". exit /b
+		%prefix64%gcc -o "build\hooks_x64.dll" hooks.c build\hooks_x64.o -mdll -lshlwapi -lcomctl32 -O2 -s
+		if not exist "build\hooks_x64.dll". exit /b
+	)
+	
+	for %%f in (%l10n%) do (
+		@echo.
+		echo Putting together %%f
+		if not exist "build\%%f\AltDrag". mkdir "build\%%f\AltDrag"
+		copy "build\AltDrag.exe" "build\%%f\AltDrag"
+		copy "build\hooks.dll" "build\%%f\AltDrag"
+		copy "localization\%%f\info.txt" "build\%%f\AltDrag"
+		copy "AltDrag.ini" "build\%%f\AltDrag"
+		"build\ini.exe" "build\%%f\AltDrag\AltDrag.ini" AltDrag Language %%f
+		if "%2" == "x64" (
+			copy "build\HookWindows_x64.exe" "build\%%f\AltDrag"
+			copy "build\hooks_x64.dll" "build\%%f\AltDrag"
+		)
+	)
+	
+	@echo.
+	echo Building installer
+	makensis /V2 installer.nsi
+) else (
+	%prefix32%gcc -o AltDrag.exe altdrag.c build\altdrag.o -mwindows -lshlwapi -lwininet -g -DDEBUG
+	%prefix32%gcc -o hooks.dll hooks.c build\hooks.o -mdll -lshlwapi -lcomctl32 -g -DDEBUG
+	
+	if "%1" == "x64" (
+		rem %prefix64%gcc -o build\unhook_x64.exe include\unhook.c
+		rem "build\unhook_x64.exe"
+		
+		%prefix64%windres -o build\hookwindows_x64.o include\hookwindows_x64.rc
+		%prefix64%windres -o build\hooks_x64.o include\hooks.rc
+		%prefix64%gcc -o HookWindows_x64.exe hookwindows_x64.c build\hookwindows_x64.o -mwindows -lshlwapi -g -DDEBUG
+		%prefix64%gcc -o hooks_x64.dll hooks.c build\hooks_x64.o -mdll -lshlwapi -lcomctl32 -g -DDEBUG
+	)
+	
+	if "%1" == "run" (
+		start AltDrag.exe
+	)
+)
---
+++ tags/altdrag-0.9/localization/en-US/installer.nsh
@@ -0,0 +1,31 @@
+;AltDrag - en-US localization by Stefan Sundin (recover89@gmail.com)
+;
+;This program is free software: you can redistribute it and/or modify
+;it under the terms of the GNU General Public License as published by
+;the Free Software Foundation, either version 3 of the License, or
+;(at your option) any later version.
+
+!insertmacro MUI_LANGUAGE "English"
+!define LANG ${LANG_ENGLISH}
+
+LangString L10N_UPGRADE_TITLE     ${LANG} "Already Installed"
+LangString L10N_UPGRADE_SUBTITLE  ${LANG} "Choose how you want to install ${APP_NAME}."
+LangString L10N_UPGRADE_HEADER    ${LANG} "${APP_NAME} is already installed on this system. Select the operation you want to perform and click Next to continue."
+LangString L10N_UPGRADE_UPGRADE   ${LANG} "&Upgrade ${APP_NAME} to ${APP_VERSION}."
+LangString L10N_UPGRADE_INI       ${LANG} "Your existing settings will be copied to ${APP_NAME}-old.ini."
+LangString L10N_UPGRADE_INSTALL   ${LANG} "&Install to a new location."
+LangString L10N_UPGRADE_UNINSTALL ${LANG} "&Uninstall ${APP_NAME}."
+LangString L10N_UPDATE_SECTION    ${LANG} "Check for update before installing"
+LangString L10N_UPDATE_DIALOG     ${LANG} "A new version is available.$\nAbort install and go to website?"
+LangString L10N_RUNNING           ${LANG} "${APP_NAME} is running. Close?"
+LangString L10N_RUNNING_UNINSTALL ${LANG} "If you choose No, ${APP_NAME} will be completely removed on next reboot."
+LangString L10N_SHORTCUT          ${LANG} "Start Menu Shortcut"
+LangString L10N_AUTOSTART         ${LANG} "Autostart"
+LangString L10N_AUTOSTART_HIDE    ${LANG} "Hide tray"
+
+LangString L10N_ALTSHIFT_TITLE    ${LANG} "Keyboard Shortcut"
+LangString L10N_ALTSHIFT_SUBTITLE ${LANG} "The shortcut Alt + Shift conflicts with ${APP_NAME}."
+LangString L10N_ALTSHIFT_HEADER   ${LANG} "The installer has detected that the Windows shortcut to toggle the current keyboard layout is Alt + Shift.$\n$\nWhen using ${APP_NAME}, you can press Shift while dragging windows to make them snap to other windows. This means you are likely to press Alt + Shift, the same combination which toggles your keyboard layout. While ${APP_NAME} internally tries to block accidentally switching the keyboard layout, it will not succeed if you press Shift before you start dragging a window.$\n$\nYou can disable or change the shortcut which toggles the keyboard layout by pressing this button. Click Next to continue."
+LangString L10N_ALTSHIFT_BUTTON   ${LANG} "&Open keyboard settings"
+
+!undef LANG
